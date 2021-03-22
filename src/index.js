496121dcef57d64ca87b0917519cba1bc97c14c2
JZims <JZimmsGames@gmail.com>
2021-03-22 15:00:51 -0400

    Added Doggos to page, Question 1

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 console.log('%c HI', 'color: firebrick')
 
-function iterateThrough (arr) {
-    console.log('hello')
-    arr.forEach(function (elem) {
-        const makeImage = document.createElement('img')
-        console.log('hello')
-    })
-}
 
-const grabDogs = () => fetch('https://dog.ceo/api/breeds/image/random/4').then(response => response.json())
-.then(data => iterateThrough(data))
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    grabDogs()
+    
     const dogImages = document.querySelector('#dog-image-container')
-
-
-
+    //console.log(dogImages)
+    const grabDogs = () => fetch('https://dog.ceo/api/breeds/image/random/4').then(response => response.json())
+.then(function(obj) {
+    console.log('hello')
+    obj.message.forEach(function (elem) {
+        const makeImage = document.createElement('img')
+        makeImage.setAttribute("alt", "src")
+        makeImage.src= elem
+        makeImage.alt= "wtf is this"
+        dogImages.append(makeImage)
+        
+    
+            
+ })
+ 
 })
+grabDogs()
+})
+ 
+
