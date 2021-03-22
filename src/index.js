fe406be1bf3f55247c981354c6b9ce40cf4a4fee
iaconom821 <iaconom821@gmail.com>
2021-03-22 14:23:14 -0400

    started project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log('%c HI', 'color: firebrick')
+
+function iterateThrough (arr) {
+    console.log('hello')
+    arr.forEach(function (elem) {
+        const makeImage = document.createElement('img')
+        console.log('hello')
+    })
+}
+
+const grabDogs = () => fetch('https://dog.ceo/api/breeds/image/random/4').then(response => response.json())
+.then(data => iterateThrough(data))
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    grabDogs()
+    const dogImages = document.querySelector('#dog-image-container')
+
+
+
+})
